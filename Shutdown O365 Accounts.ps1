acfbc448c3111a86dc445e1d42c6510054a9cfe6
ploguidic3 <ploguidic3@gmail.com>
2019-11-21 11:21:06 -0500

    Update Shutdown O365 Accounts.ps1

--- Shutdown O365 Accounts.ps1
+++ Shutdown O365 Accounts.ps1
@@ -1,13 +1,9 @@
-#NOTE: If you got CST to add the separated worker's email to the report you'll need to run a find and replace against that column with find set to "@bayada.com" and replace blank, this will conver the e-mail to the SamAccuntName variable
-#you'll also need to rename the column to "samaccountname" or change the variable name in the script below
-#This script has to be run from a privliged account, use the Admin file explorer trick to run it with necessary priviliges
-#You must copy the ForwardEnd CSV to the same path this script runs in, open the CSV in Notepad, copy the contents, create a new text file in the directory in file explorer admin, past the contents
-#and rename your new text file ForwardEnd.csv
+#N
 #
 #Pulls in the CSV (replace jloguidice-a with your -a account)
-$csv = Import-csv "C:\Users\jloguidice-a\Documents\Scripts\ForwardEnd.csv"
+$csv = Import-csv "C:\Users\%username%\Documents\Termination Reports\Terminated Users.csv"
 #Sets TargetOU variable as Bayada Disabled Users
-$TargetOU = "OU=Bayada Disabled Users,DC=heroes,DC=bayada,DC=com"
+$TargetOU = "OU=Example OU,DC=Example DC,DC=Example DC,DC=Example DC"
 ##Loops through csv
 for($i=0; $i -lt $csv.Count; $i++){
 #Pulls User Distinguished Name based on SamAccountName
