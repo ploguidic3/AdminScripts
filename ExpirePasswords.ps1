88875165096f53e639c4885553204ff6d1a287a3
ploguidic3 <ploguidic3@gmail.com>
2019-11-21 13:16:21 -0500

    Mass Remove "Password Never Expires" attribute
    Mass Remove "Password Never Expires" attribute

---
+++ ExpirePasswords.ps1
@@ -0,0 +1,10 @@
+﻿# Import CSV and set variable
+$csv = Import-Csv "C:\scripts\PasswordExpire.csv"
+# For loop to iterate through the csv.
+for($i=0; $i -lt $csv.Count; $i++){
+#Sets accounts to expire
+Set-ADUser -Identity $($csv[$i].SamAccountName) -PasswordNeverExpires:$False
+}
+Write-Host " "$csv.Count" account passwords set to expire. Press any key to exit..."
+#Exits when you press any key
+$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") 
